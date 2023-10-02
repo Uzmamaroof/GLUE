c47b36db6089640a878d795d22a86874afdf03fc
Michael Wrana <michaelwrana@adsl-172-10-1-21.dsl.sndg02.sbcglobal.net>
2023-10-02 16:15:14 +0900

    fix spacing

--- pipeline_final_v2.py
+++ pipeline_final_v2.py
@@ -27,10 +27,10 @@ if __name__ == '__main__':
 
 
     try:
-    mp.set_start_method('spawn', force=True)
-    print("Spawned")
+        mp.set_start_method('spawn', force=True)
+        print("Spawned")
     except RuntimeError:
-    pass
+        pass
 
 
     # In[ ]:
