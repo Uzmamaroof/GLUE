8f0a3bbf1a2423f1ebaea554bf9f75231a5485cb
Michael Wrana <michaelwrana@Michaels-MBP-2.lan>
2023-11-08 18:14:15 -0500

    add help files

---
+++ GridSearch_Tick2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pickle
+from multiprocessing import Pool
+
+# PART 2
+if __name__ == '__main__':
+    
+    global traces
+
+    with open('../nonzero_traces.npy', 'rb') as f:
+        traces = pickle.load(f)
+    
+    shapelet_samples_list = [4, 5, 6]
+    shapelet_size_list = [300, 400, 500, 600]
+    clf_samples_list = [600000]
+
+    from utils import evaluate_parameters
+    
+    parameter_list = np.array(np.meshgrid(shapelet_samples_list, shapelet_size_list, clf_samples_list)).T.reshape(-1,3)
+    
+    with Pool(4) as p:
+        p.map(evaluate_parameters, parameter_list)
